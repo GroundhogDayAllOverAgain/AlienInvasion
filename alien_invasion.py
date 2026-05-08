1d3a53cd56ee9017543e6f5ab0d91c44c04aa4e1
GroundhogDayAllOverAgain <joethepimpernel@gmail.com>
2026-05-08 18:20:35 -0500

    Fullscreen mode added

--- alien_invasion.py
+++ alien_invasion.py
@@ -12,9 +12,11 @@ class AlienInvasion:
         pygame.init()
         self.clock = pygame.time.Clock()
         self.settings = Settings()
-        self.screen = pygame.display.set_mode(
-            (self.settings.screen_width, self.settings.screen_height)
-        )
+
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
+
         pygame.display.set_caption("Alien Invasion")
 
         # Set the background color.
@@ -31,7 +33,7 @@ class AlienInvasion:
             self._update_screen()
 
     def _check_events(self):
-        # Watch for keyboard and mouse events.
+        """Respond to keyboard and mouse events."""
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
@@ -57,12 +59,11 @@ class AlienInvasion:
             self.ship.moving_left = False
 
     def _update_screen(self):
+        """Update images on the screen, and flip to the new screen."""
         # Redraw the screen during each pass through the loop.
         self.screen.fill(self.bg_color)
-
         # Draw the ship
         self.ship.blitme()
-
         # Make the most recently drawn screen visible.
         pygame.display.flip()
         self.clock.tick(60)
