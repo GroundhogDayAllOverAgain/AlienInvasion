8f2c6caa90a1bec3fd83345a40511fc57f0612e6
GroundhogDayAllOverAgain <joethepimpernel@gmail.com>
2026-05-08 21:21:17 -0500

    Rocket movement speed setting changed from 1.5 to 10.

--- settings.py
+++ settings.py
@@ -8,7 +8,7 @@ class Settings:
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
         # Ship settings
-        self.ship_speed = 1.5
+        self.ship_speed = 10
 
         # Bullet settings
         self.bullet_speed = 2.0
